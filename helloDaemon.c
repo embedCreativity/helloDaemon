ea6f818e9ba493eca117d26d248f6fba40ba5e8e
EmbedCreativity <embedCreativity@users.noreply.github.com>
2019-06-05 14:26:13 -0400

    Update helloDaemon.c
    Omit system call for a device that may not allow a system call from within the binary.

--- helloDaemon.c
+++ helloDaemon.c
@@ -4,6 +4,6 @@
 int main (void)
 {
     printf("Hello Daemon!\n");
-    system("echo HelloDaemon > /tmp/helloDaemon");
+    //system("echo HelloDaemon > /tmp/helloDaemon");   <-- testing on a device that may not allow this
     return 0;
 }
